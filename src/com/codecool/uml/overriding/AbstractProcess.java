a3a87891609b03a501adff68c622e69d747c5df5
kinga00099 <kinga00099@gmail.com>
2018-10-24 10:25:13 +0200

    abstract class implemented

--- src/com/codecool/uml/overriding/AbstractProcess.java
+++ src/com/codecool/uml/overriding/AbstractProcess.java
@@ -1,9 +1,21 @@
 package com.codecool.uml.overriding;
 
-public class AbstractProcess {
+public abstract class AbstractProcess {
 
-    public void process(Orderable item){}
-    public void stepBefore(){}
-    protected void action(Orderable item){}
-    public void stepAfter(){}
+    abstract protected void action(Orderable item);
+
+    public void process(Orderable item){
+        this.stepBefore();
+        this.action(item);
+        this.stepAfter();
+    }
+
+    public void stepBefore(){
+        System.out.println("Step before");
+    }
+
+    public void stepAfter(){
+        System.out.println("Step after");
+    }
+
 }
